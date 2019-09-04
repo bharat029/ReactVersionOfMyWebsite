3f2ae4bc8d1de049166b034e482dcc93a91799c2
Bharathan Mudaliar <vbharathan.2013@gmail.com>
2019-09-05 02:08:05 +0530

    lazy and pwa

--- public/worker.js
+++ public/worker.js
@@ -1,47 +1,51 @@
-const staticCacheName = 'site-static-v1';
-const dynamicCacheName = 'site-dynamic-v1';
+const staticCacheName = 'site-static-v4'
 
 const assets = [
   '/',
-  '/index.html',
-];
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
+  'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
+  'https://code.jquery.com/jquery-3.2.1.slim.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js',
+  'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'
+]
 
 // install event
 self.addEventListener('install', evt => {
-  // console.log('service worker installed');
+  //console.log('service worker installed')
   evt.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-      console.log('caching shell assets');
-      cache.addAll(assets);
+    caches.open(staticCacheName).then(cache => {
+      console.log('caching shell assets')
+      cache.addAll(assets)
     })
-  );
-});
+  )
+})
 
 // activate event
 self.addEventListener('activate', evt => {
-  // console.log('service worker activated');
+  //console.log('service worker activated')
   evt.waitUntil(
     caches.keys().then(keys => {
-      // console.log(keys);
+      //console.log(keys)
       return Promise.all(keys
-        .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+        .filter(key => key !== staticCacheName)
         .map(key => caches.delete(key))
-      );
+      )
     })
-  );
-});
+  )
+})
 
 // fetch event
-self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request).then(fetchRes => {
-        return caches.open(dynamicCacheName).then(cache => {
-          cache.put(evt.request.url, fetchRes.clone());
-          console.log(evt.request.url);
-          return fetchRes;
+self.self.addEventListener('fetch', evt => {
+  if(evt.request.url.indexOf('firestore.googleapis.com') === -1){
+    evt.respondWith(
+      caches.match(evt.request).then(cacheRes => {
+        return cacheRes || fetch(evt.request).then(fetchRes => {
+          return caches.open(dynamicCacheName).then(cache => {
+            cache.put(evt.request.url, fetchRes.clone())
+            return fetchRes
+          })
         })
-      });
-    })
-  );
-});
+      })
+    )
+  }
+})
--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, {Component} from 'react'
+import React, { Component, lazy, Suspense } from 'react'
 import './App.css'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import SideBar from './components/Sidebar'
 import Navbar from './components/Navbar'
-import Aboutme from './components/AboutMe'
-import ProjectsListView from './components/ProjectsListView'
-import ProjectsDetailedView from './components/ProjectsDetailedView'
-import Courses from './components/Courses'
-import Cv from './components/CV'
-import Admin from './components/Admin/Admin'
-import SignIn from './components/Admin/SignIn'
+
+const Aboutme = lazy(() => import('./components/AboutMe'))
+const ProjectsListView = lazy(() => import('./components/ProjectsListView'))
+const ProjectsDetailedView = lazy(() => import('./components/ProjectsDetailedView'))
+const Courses = lazy(() => import(() => import('./components/Courses')))
+const CV = lazy(() => import('./components/CV'))
+const Admin = lazy(() => import('./components/Admin/Admin'))
+const SignIn = lazy(() => import('./components/Admin/SignIn'))
 
 class App extends Component { 
   render() {
@@ -19,14 +20,16 @@ class App extends Component {
           <SideBar />
           <Navbar />
           <div className="col-md-9 offset-md-3" id="main-content">
-            <Route exact path="/" component={Aboutme} />
-            <Route exact path="/projects" component={ProjectsListView} />
-            <Route exact path="/projects/:projectID" component={ProjectsDetailedView} />
-            <Route exact path="/courses/" component={Courses} />
-            <Route exact path="/cv/" component={Cv} />
-            <Route path="/admin/" component={Admin} />
-            <Route exact path="/signin/" component={SignIn} />
-            <br /><br /><br />
+            <Suspense fallback="<div class='h2'>Loadiing...</div>">
+              <Route exact path="/" component={Aboutme} />
+              <Route exact path="/projects" component={ProjectsListView} />
+              <Route exact path="/projects/:projectID" component={ProjectsDetailedView} />
+              <Route exact path="/courses/" component={Courses} />
+              <Route exact path="/cv/" component={CV} />
+              <Route path="/admin/" component={Admin} />
+              <Route exact path="/signin/" component={SignIn} />
+              <br /><br /><br />
+            </Suspense>
           </div>
         </Router>
       </>

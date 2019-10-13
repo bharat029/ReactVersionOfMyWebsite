ce4dfa8913ca99a89cb134b33ace8e67a62192e8
Bharathan Mudaliar <vbharathan.2013@gmail.com>
2019-10-13 18:16:09 +0530

    no styles in index and card layout for projects

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
         <div className="nav-wrapper container">
           <div className="row">
             <h1 role="button" onClick={e => document.querySelector('#sidebar-container').classList.toggle('menu-displayed')} className="center-align" id="title">Bharathan Mudaliar</h1>
-            <button id='menu' onClick={toggleMenu} className="col s10 m6 hide-on-large-only"><i className="material-icons">menu</i></button>
+            <button id='menu-btn' onClick={toggleMenu} className="col s10 m6 hide-on-large-only"><i className="material-icons">menu</i></button>
             <ul id="menu-content" className="col m6 right hide-on-med-and-down">
               <li><NavLink exact to="/" className="center-align">About Me</NavLink></li>
               <li><NavLink to="/projects" className="center-align">Projects</NavLink></li>
--- src/components/ProjectsListView.js
+++ src/components/ProjectsListView.js
@@ -24,7 +24,8 @@ const ProjectListView = ({ projects }) => {
         <ul className="row project-row">
           {
             projects 
-            ? projects.map(project =><li key={project.id} className="col m6 s12"><Link className="white-text card grey darken-3 center-align project" to={"/projects/" + project.id}><h5 className="card-title">{project.title}</h5><p className="proj-desc">{project['s-desc']}</p></Link></li>) 
+            // ? projects.map(project =><li key={project.id} className="col m6 s12"><Link className="white-text card grey darken-3 center-align project" to={"/projects/" + project.id}><h5 className="card-title">{project.title}</h5><p className="proj-desc">{project['s-desc']}</p></Link></li>) 
+            ? projects.map(project =><li key={project.id} className="col l6 s12"><div className="white-text card grey darken-2"><div className="card-content"><h5 className="card-title truncate">{project.title}</h5><p>{project['s-desc']}</p><div className="card-action"><Link className="grey-text text-lighten-2" to={`/projects/${project.id}`}>Read More...</Link></div></div></div></li>) 
             : <p>Loading Data... Please wait!!</p>
           }
         </ul>
